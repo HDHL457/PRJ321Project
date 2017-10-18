90763f2c54db2c037a73f3903629b372a128af49
yanyagin <hienndse05142@fpt.edu.vn>
2017-10-18 15:12:40 +0700

    Tree entity
    add tree entity in package com.entity

---
+++ Project PRJ321/src/java/com/entity/Tree.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.entity;
+
+/**
+ *
+ * @author YAGIN
+ */
+public class Tree {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    private String name, id;
+    private double price;
+
+    public Tree() {
+    }
+
+    public Tree(String name, String id, double price) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+    }
+    
+    }
